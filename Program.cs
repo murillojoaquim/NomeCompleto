193bd9217b6908856ed2e636f0b1611a653cc5a1
murillojoaquim <murillojalves@hotmail.com>
2021-09-03 20:03:36 -0300

    Nome Completo

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Digite seu primeiro nome: ");
+            
+            string primeironome = Console.ReadLine();
+
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("Digite seu sobrenome: ");
+
+            string sobrenome = Console.ReadLine();
+
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("Nome Completo: ");
+            Console.WriteLine($"{primeironome} {sobrenome}");
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("Nome de Catálogo: ");
+            Console.Write(sobrenome.ToUpper());
+            Console.Write(", ");
+            Console.WriteLine(primeironome);
+
+            Console.ResetColor();
+
+        }
+    }
+}
